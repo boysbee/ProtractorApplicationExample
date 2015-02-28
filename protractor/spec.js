0a158a579630d5daed9c8aeba04da22234a7decb
Filipe Falcão <filipebatista@ic.ufal.br>
2015-02-28 04:56:01 -0300

    Revert "Revert "Revert "Protractor tests"""
    This reverts commit 105bfb58a9f76fe57f22b1b586651e8b57ce5b18.

--- protractor/spec.js
+++
@@ -1,7 +0,0 @@
-describe('project homepage title', function() {
-    it('should have a title', function() {
-        var url = "https://github.com/FilipeFalcaoBatista.github.io/ProtractorApplicationExample";                 browser.get(url);
-
-        expect(browser.getTitle()).toEqual('AngularJS and Protractor');
-    });
-});
